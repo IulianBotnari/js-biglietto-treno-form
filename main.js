c5f971ca066130adadd7b583be47308827032d34
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-09 13:54:21 +0200

    Add calc bilet price

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 //mistone1
 
-let passengerAge = 10//Number(prompt("Inserisci età del passegero"))
-
-let distanceKm = 10//Number(prompt("Inserisci età del passegero"))
-
-let priceForKm = 0.21
-
-let disconut20 = 0.20
-
-let disconut40 = 0.40
-
-console.log(passengerAge, distanceKm, priceForKm, disconut20, disconut40);
 
 let biletPrice = document.getElementById("biletPrice").innerHTML
 
+let priceBilet = calcBiletPrice()
+console.log(priceBilet);
+
+function calcBiletPrice() {
+
+    let passengerAge = 10//Number(prompt("Inserisci età del passegero"))
+
+    let distanceKm = 10//Number(prompt("Inserisci età del passegero"))
+    
+    let priceForKm = 0.21
+    
+    let disconut20 = 0.20
+    
+    let disconut40 = 0.40
+
+    let clacPrice = (distanceKm * priceForKm)
+
+    return clacPrice
+    
+    
+}
