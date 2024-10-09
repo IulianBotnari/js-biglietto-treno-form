22c94ee01bc8821deb4dc4230441d64b18d76e9f
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-09 16:25:33 +0200

    Finish
    sono impegnato adesso, se avrò tempo modifichero lo stile della pagina in seguito

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ let calcButton = document.getElementById("calc")
 calcButton.addEventListener("click", calcBiletPrice)
 
 
+
 function calcBiletPrice() {
 
     let passengerAge = document.getElementById("passengerAge")
+
+    let passengerName = document.getElementById("nome_cognome")
+
+    let passName = passengerName.value
+    document.getElementById("passengerName").innerHTML = passName
 
     let ageValue = passengerAge.value
 
@@ -30,16 +36,32 @@ function calcBiletPrice() {
 
     let clacPrice = (distanceKmValue * priceForKm)
 
+    let carrozza = Math.floor(Math.random() * 9) + 1
+    let b_cod = Math.floor(Math.random() * 10000) + 1
     if (ageValue === "Minorenne") {
 
         clacPrice = clacPrice - (clacPrice * disconut20)
-        return document.getElementById("costBilet").innerHTML = clacPrice.toFixed(2)
+        document.getElementById("bilet_type").innerHTML = "Biglietto ridotto 20%"
+        document.getElementById("carrozza").innerHTML = carrozza
+        document.getElementById("b_cod").innerHTML = b_cod
+        document.getElementById("costBilet").innerHTML = clacPrice.toFixed(2)
     }else if (ageValue === "Anziano") {
         clacPrice = clacPrice - (clacPrice * disconut40)
-        return document.getElementById("costBilet").innerHTML = clacPrice.toFixed(2)
+        document.getElementById("bilet_type").innerHTML = "Biglietto ridotto 40%"
+        document.getElementById("costBilet").innerHTML = clacPrice.toFixed(2)
     } else {
-        return document.getElementById("costBilet").innerHTML = clacPrice.toFixed(2)
+        document.getElementById("bilet_type").innerHTML = "Biglietto Standard"
+        document.getElementById("costBilet").innerHTML = clacPrice.toFixed(2)
     }
+
+
+    
 
 }
 
+let annulla = document.getElementById("annulla")
+
+annulla.addEventListener('click', function() {
+    
+    location.reload();
+})
