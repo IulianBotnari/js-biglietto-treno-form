7f4d5c95e07bc930414868a9314f46e97c53e132
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-09 13:45:13 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//mistone1
+
+let passengerAge = 10//Number(prompt("Inserisci età del passegero"))
+
+let distanceKm = 10//Number(prompt("Inserisci età del passegero"))
+
+let priceForKm = 0.21
+
+let disconut20 = 0.20
+
+let disconut40 = 0.40
+
+console.log(passengerAge, distanceKm, priceForKm, disconut20, disconut40);
